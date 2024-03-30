4ac2bfa8afb58344da74daa9a5adcd867d99cfbc
shivam patel <shivampatel6514@gmail.com>
2024-03-30 20:55:43 +0530

    add codde

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,9 @@
 from django.urls import include, path
 
 from . import views
-# from .feeds import AtomSiteNewsFeed, LatestPostsFeed
 
 urlpatterns = [
-    # path("feed/rss", LatestPostsFeed(), name="post_feed"),
-    # path("feed/atom", AtomSiteNewsFeed()),
     path("", views.PostList, name="home"),
-    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path("<slug:slug>/", views.post_detail, name="post_detail"),
     path('like-comment/<int:comment_id>/', views.like_comment, name='like_comment'),
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,22 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
-# import debug_toolbar
 
-# from blog.sitemaps import PostSitemap
-
-# sitemaps = {
-#     "posts": PostSitemap,
-# }
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
-    path("summernote/", include("django_summernote.urls")),
-    # path('__debug__/', include(debug_toolbar.urls)),
-    # path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
 
 if settings.DEBUG:
